31e2e7b1adb13690f6b24e68e97b8218db598843
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-04-06 06:05:39 +0300

    task-08
    done

--- homework-07/src/js/task-08.js
+++ homework-07/src/js/task-08.js
@@ -1,22 +1,40 @@
-// Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит
-// количество элементов в `input` и нажимает кнопку `Создать`, после чего
-// рендерится коллекция. При нажатии на кнопку `Очистить`, коллекция элементов
-// очищается.
+const divControlsEl = document.getElementById('controls');
+const divBoxesEl = document.getElementById('boxes');
+const inputEl = divControlsEl.querySelector('input');
+const destroyBtnEl = document.querySelector('[data-action="destroy"]');
+const renderBtnEl = document.querySelector('[data-action="render"]');
 
-// Создай функцию `createBoxes(amount)`, которая принимает 1 параметр `amount` -
-// число. Функция создает столько `div`, сколько указано в `amount` и добавляет их
-// в `div#boxes`.
-
-// Каждый созданный div:
-
-// - Имеет случайный rgb цвет фона
-// - Размеры самого первого div - 30px на 30px
-// - Каждый следующий div после первого, должен быть шире и выше предыдущего на
-//   10px
-
-// Создай функцию `destroyBoxes()`, которая очищает `div#boxes`.
+console.log(divBoxesEl);
 
 
-function createBoxes(amount) {
+
+const createBoxes = function (amount) {
+    for (let i = 1; i <= amount; i += 1) {
+        divBoxesEl.insertAdjacentHTML('beforeend', '<div class = "newDiv"></div>') 
+    }
+};
+const onRenderBtnClick = function () {
+    createBoxes(inputEl.value);
+    const divsEl = divBoxesEl.querySelectorAll('.newDiv');
     
+    let size = 20;
+    divsEl.forEach(element => {
+        size += 10;
+        const sizeValue = `${size}px` 
+        element.style.width = sizeValue;
+        element.style.height = sizeValue;
+
+        element.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
+    });
+
+function getRandom(min, max){
+  return Math.ceil(Math.random() * ((max - min) + min))
 }
+};
+
+const ondestroyBtnClick = function() {
+    divBoxesEl.innerHTML = '';
+};
+
+destroyBtnEl.addEventListener('click', ondestroyBtnClick);
+renderBtnEl.addEventListener('click', onRenderBtnClick);
