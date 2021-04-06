4f889b466f955744102388fb2a2dd63161372194
Stas Kotelnykov <staskotelnykov@gmail.com>
2021-04-06 20:14:47 +0300

    finish

--- homework-07/src/js/task-08.js
+++ homework-07/src/js/task-08.js
@@ -4,10 +4,6 @@ const inputEl = divControlsEl.querySelector('input');
 const destroyBtnEl = document.querySelector('[data-action="destroy"]');
 const renderBtnEl = document.querySelector('[data-action="render"]');
 
-console.log(divBoxesEl);
-
-
-
 const createBoxes = function (amount) {
     for (let i = 1; i <= amount; i += 1) {
         divBoxesEl.insertAdjacentHTML('beforeend', '<div class = "newDiv"></div>') 
@@ -17,29 +13,22 @@ const onRenderBtnClick = function () {
     if (inputEl.value <= 100) {
         createBoxes(inputEl.value);
     const divsEl = divBoxesEl.querySelectorAll('.newDiv');
-    
     let size = 20;
     divsEl.forEach(element => {
-        size += 10;
-        const sizeValue = `${size}px` 
-        element.style.width = sizeValue;
-        element.style.height = sizeValue;
-
+        size += 10; 
+        element.style.width = `${size}px`;
+        element.style.height = `${size}px`;
         element.style.backgroundColor = `rgb(${getRandom(0, 255)}, ${getRandom(0, 255)}, ${getRandom(0, 255)})`;
     });
     } else {
         inputEl.classList.add('invalid')
-    }
-    
-
+    };
 function getRandom(min, max){
   return Math.ceil(Math.random() * ((max - min) + min))
-}
+    }
 };
-
 const ondestroyBtnClick = function() {
     divBoxesEl.innerHTML = '';
 };
-
 destroyBtnEl.addEventListener('click', ondestroyBtnClick);
 renderBtnEl.addEventListener('click', onRenderBtnClick);
